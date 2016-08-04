96983079cecf3f9a9ebe6c9995ac18edd76b4455
Robiul Islam <robiul.islam.uk@live.co.uk>
2016-08-05 00:59:17 +0100

    initial checkin

---
+++ Log4NetDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using Autofac;
+using log4net;
+using log4net.Config;
+using System.Linq;
+using System.Reflection;
+
+namespace Log4NetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<LoggingModule>();
+            builder.RegisterType<ADemoService>().As<IAmAService>();
+            var container = builder.Build();
+
+
+            var service = container.Resolve<IAmAService>();
+            service.DisplayWord();
+           
+        }
+    }
+
+
+    public interface IAmAService
+    {
+        void DisplayWord();
+    }
+    public class ADemoService: IAmAService
+    {
+        ILog logger;
+        public ADemoService(ILog logger)
+        {
+            this.logger = logger;
+        }
+
+        public void DisplayWord()
+        {
+
+            logger.Info("testing");
+        }
+    }
+
+}
